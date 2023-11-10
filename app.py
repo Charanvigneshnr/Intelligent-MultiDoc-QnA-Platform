d584aad1b2971ccfde45a3c3b6b28e1355577600
Charanvigneshnr <charanvignesh10@gmail.com>
2023-11-10 23:03:19 +0530

    Updated with a better reliable model Replicate's LLAMA2 model

--- app.py
+++ app.py
@@ -71,9 +71,9 @@ def main():
     initialize_session_state()
     st.title("Intelligent MultiDoc QnA Platform :books:")
     st.sidebar.title("Upload your documents to process")
+    uploaded_files = st.sidebar.file_uploader("Upload files", accept_multiple_files=True)
     st.sidebar.write("After your prompt, please wait, if the response is small then it will be fast, else it will "
                      "take some time as it is generating the full string then it prints out the response")
-    uploaded_files = st.sidebar.file_uploader("Upload files", accept_multiple_files=True)
     if uploaded_files:
         text = []
         for file in uploaded_files:
